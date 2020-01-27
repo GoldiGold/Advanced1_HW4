d709595938e984e391f06551bdd669a97b3655c7
chen goldi <17chen07@gmail.com>
2020-01-27 18:56:45 +0200

    CG 27.11 18:55 trying to make the timeout

--- MySerialServer.cpp
+++ MySerialServer.cpp
@@ -2,10 +2,12 @@
 // Created by חן גולדשטיין on 27/01/2020.
 //
 
+#include <thread>
 #include "MySerialServer.h"
 
 int MySerialServer::open(int port, ClientHandler c) {
 	sockaddr_in serv_addr;
+	int startTime, endTime;
 	this->sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd < 0) {
 		std::cerr << "\n Socket creation error \n" << std::endl;
@@ -21,23 +23,30 @@ int MySerialServer::open(int port, ClientHandler c) {
 		std::cerr << "bind failed" << std::endl;
 		exit(EXIT_FAILURE);
 	}
-	if (listen(this->sockfd, 1) < 0) {
+	if (listen(this->sockfd, 5) < 0) {
 		std::cerr << "listen error" << std::endl;
 		exit(EXIT_FAILURE);
 	}
-	if ((new_socket = accept(this->sockfd, (struct sockaddr *) (&serv_addr), (socklen_t *) (&serv_addr))) < 0) {
-		std::cerr << "accept error" << std::endl;
-		exit(EXIT_FAILURE);
-	} else {
-		this->clients_queue.push(new_socket);
+	struct timeval tv;
+	tv.tv_sec = 120;
+	while (tv.tv_sec > 0) {
+		setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (const char *) &tv, sizeof tv);
+		if ((new_socket = accept(this->sockfd, (struct sockaddr *) (&serv_addr), (socklen_t *) (&serv_addr))) < 0) {
+			std::cerr << "accept error" << std::endl;
+			exit(EXIT_FAILURE);
+		} else {
+			this->clients_queue->push(new_socket);
+		}
+//		std::thread clientHandler(c.handleCLient, this->clients_queue->front());
+//		clientHandler.join();
+		close(new_socket);
 	}
 ////	TODO: CHECK IF YOU NEED TO OPEN A THREAD FOR SOMETHING, IF YES THEN FOR WHAT' AND WHAT DO YOU DO WITH THE
 //// 	TODO: CLIENT_HANDLER IN THIS FUNCTION. in the end need to close the client's new socket.
 //	std::thread openServer(acceptClient, this);
 //	openServer.detach();
 
-	close(new_socket);
-	return new_socket;
+	return 1;
 }
 
 int MySerialServer::stop() {
--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <sys/time.h>
 
 int main() {
 	std::cout << "Hello, World! VERSION 2.0" << std::endl;
+
+	struct timeval tvBefore;
+	struct timeval tvAfter;
+//	tv.tv_sec = 10;
+	gettimeofday(&tvBefore, NULL);
+	std::cout << " time try:" << tvBefore.tv_sec << std::endl;
+//	while(tv.tv_sec > gettimeofday(&tv, NULL)){
+////		if (tv.tv_sec % 10 == 0){
+////			std::cout << "thing" << std::endl;
+////		}
+//	}
+	int i = 0;
+	while (i < 1000000000) {
+		++i;
+	}
+	gettimeofday(&tvAfter, NULL);
+	std::cout << "time ended: " << tvAfter.tv_sec << " i is : " << i << std::endl;
 	return 0;
 }
