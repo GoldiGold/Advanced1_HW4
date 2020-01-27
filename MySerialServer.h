efb2cce2d35f67bea854575ab15a9f553f941e3b
chen goldi <17chen07@gmail.com>
2020-01-27 03:00:09 +0200

    created the basic classes for the first part of the ex - part 5 still has a linker problem

--- MySerialServer.h
+++ MySerialServer.h
@@ -8,14 +8,14 @@
 
 //#include <queue>
 
-class MySerialServer: public Server {
+class MySerialServer : public Server {
  private:
 //	std::queue<int> clients_queue;
 	int sockfd; // lets make it so it will take only one client (anyway we need PARALLEL)
 
 
-	int open(int port, ClientHandler c) override ;
-	virtual int stop () override ;
+	int open(int port, ClientHandler c);
+	virtual int stop();
 };
 
 #endif //ADVANCED1_HW4__MYSERIALSERVER_H_
--- Server.h
+++ Server.h
@@ -5,10 +5,10 @@
 #ifndef ADVANCED1_HW4__SERVER_H_
 #define ADVANCED1_HW4__SERVER_H_
 #include "ClientHandler.h"
-#include <sys/socket.h>
+//#include <sys/socket.h>
 #include <unistd.h>
 #include <netinet/in.h>
-#include <sstream>
+//#include <sstream>
 #include <iostream>
 
 
