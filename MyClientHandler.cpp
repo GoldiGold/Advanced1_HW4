4db9526c5d8ecaac274a0909fc84ab7a1b517a84
chen goldi <17chen07@gmail.com>
2020-01-30 22:17:14 +0200

    YST 30.1  - 22:16 everything works

--- MyClientHandler.cpp
+++ MyClientHandler.cpp
@@ -141,7 +141,6 @@ MatrixProblem *MyClientHandler::createMatProblem(const std::vector<std::string> 
 		++line_index;
 		temp.clear();
 	}
-
 	int start_x, start_y, end_x, end_y;
 	try {
        start_x = stoi(mat[start_index][0]);
@@ -165,15 +164,7 @@ MatrixProblem *MyClientHandler::createMatProblem(const std::vector<std::string> 
     }
 	mat.erase(mat.begin() + start_index, mat.end());
 
-//	for (const std::vector<std::string> &vec: mat) {
-//		for (const std::string &str : vec) {
-//			std::cout << "arg: " << str;
-//		}
-//		std::cout << std::endl;
-//	}
-
 	std::cout << "size:::: " << mat.size() << std::endl;
-
 	std::vector<std::vector<int>>* matrix = new std::vector<std::vector<int>>;
 	std::vector<int> temp_int;
 	matrix->reserve(mat.size());
@@ -189,114 +180,10 @@ MatrixProblem *MyClientHandler::createMatProblem(const std::vector<std::string> 
 		matrix->push_back(temp_int);
 		temp_int.clear();
 	}
-
 	std::cout << "size of matrix: " << matrix->size() << std::endl;
-
 	return new MatrixProblem(matrix, start_x, start_y, end_x, end_y);
 }
+
 MyClientHandler *MyClientHandler::clone() {
 	return new MyClientHandler(this->solver->clone(), this->cache_manager->clone());
 }
-//std::vector<std::string> MyClientHandler::findStartCo(std::vector<std::string> problem) {
-//	int start_index = 0;
-//	int end_index = 0;
-//	for(int index = 0; index < problem.size(); ++index){
-//
-//	}
-//	return std::vector<std::string>();
-//}
-
-
-
-
-
-
-
-
-
-
-
-//{
-//	std::cout<< "handling client right now" << std::endl;
-//	char buffer[BUFFER_SIZE];
-////		std::vector<std::string> sub_n;
-//	std::string t;
-//	std::string problem = nullptr;
-//	int counter = 0;
-//	int i = 0;
-//	do {
-//		// TODO: UNDERSTAND WHY WE AIN'T GETTING ANY VALUES FROM THE CLIENT (MAYBE SOMETHING WITH THE INCLUDED HEADERS)
-//		ssize_t size = read(sockfd, buffer, BUFFER_SIZE);
-//		t = buffer;
-//		std::cout << buffer << std::endl;
-//		problem.append(buffer);
-//		if (t.find('\n') != std::string::npos) {
-//			++counter;
-//		}
-//		++i;
-//		std::cout << i << std::endl;
-////			sub_n = split(t, "\n");
-//	} while (t.find("end") == std::string::npos);
-//
-//	int endIndex = problem.find("end\n");
-//	problem.erase(endIndex, strlen("end\n"));
-//
-//	std::cout << problem << std::endl;
-////	std::string sol = this->solver->solve("end");
-////	this->cache_manager->insert("end", sol);
-//	std::vector<std::string> lines = split(problem, '\n');
-//
-//// TODO: REMEMBER THAT THE LAST TWO LINES ARE OF THE START-CO AND END-CO.
-//
-//	std::cout << "the amount of lines is: " << counter << std::endl;
-//	send(sockfd, "hello", strlen("hello"), 0);
-//	send(sockfd, "fuckkkkk", strlen("fuckkkkk")/*sol.c_str(), sol.length()*/, 0);
-////		write(sockfd, sol.c_str(), ((size_t)sol.length()));
-//	std::cout << "message was sent" << std::endl;
-//	close(sockfd);
-//
-////		std::cout << "solution from file: " << this->cache_manager->get_solution("end") << std::endl;
-//
-//	return 1;
-//}
-
-
-
-
-
-
-
-
-
-
-
-//int MyClientHandler::handleClient(int sockfd) {
-//	char buffer[BUFFER_SIZE];
-////		std::vector<std::string> sub_n;
-//	std::string t;
-//	int counter = 0;
-//	int i = 0;
-//	do {
-//		ssize_t size = read(sockfd, buffer, BUFFER_SIZE);
-//		t = buffer;
-////			std::cout << buffer << std::endl;
-//		if (t.find('\n') != std::string::npos) {
-//			++counter;
-//		}
-//		++i;
-////			sub_n = split(t, "\n");
-//	} while (t.find("end") == std::string::npos);
-////	std::string sol = this->solver->solve("end");
-////	this->cache_manager->insert("end", sol);
-//
-//	std::cout << "the amount of lines is: " << counter << std::endl;
-//	send(sockfd, "hello", strlen("hello"), 0);
-////	send(sockfd, sol.c_str(), sol.length(), 0);
-////		write(sockfd, sol.c_str(), ((size_t)sol.length()));
-//	std::cout << "message was sent" << std::endl;
-//	close(sockfd);
-//
-//	std::cout << "solution from file: " << this->cm->get_solution("end") << std::endl;
-//
-//	return 1;
-//}
--- main.cpp
+++ main.cpp
@@ -1,51 +1,17 @@
 #include <iostream>
-//#include <sys/time.h>
-#include "MySerialServer.h"
 #include "MyParallelServer.h"
 #include "MyClientHandler.h"
 #include "FileCacheManager.h"
-#include "ShitSolver.h"
-#include "BreadthFirstSearch.h"
-#include "MyCmp.h"
 #include "SolverSearcher.h"
 #include "BestFirstSearch.h"
 #include "AStarCmp.h"
-//#include "MyTestClientHandler.h"
 
 int main() {
 	std::cout << "Hello, World! VERSION 2.0" << std::endl;
 
-//	struct timeval tvBefore;
-//	struct timeval tvAfter;
-////	tv.tv_sec = 10;
-//	gettimeofday(&tvBefore, NULL);
-//	std::cout << " time try:" << tvBefore.tv_sec << std::endl;
-////	while(tv.tv_sec > gettimeofday(&tv, NULL)){
-//////		if (tv.tv_sec % 10 == 0){
-//////			std::cout << "thing" << std::endl;
-//////		}
-////	}
-//	int i = 0;
-//	while (i < 1000000000) {
-//		++i;
-//	}
-//	gettimeofday(&tvAfter, NULL);
-//	std::cout << "time ended: " << tvAfter.tv_sec << " i is : " << i << std::endl;
-
 	auto my_p_server = new MyParallelServer();
 	auto cache = new FileCacheManager();
-	auto string_cache = new FileCacheManager();
-	my_p_server->open(5400, /*new MyTestClientHandler<std::string, std::string>(new ShitSolver(), string_cache)*/
-						   new MyClientHandler(new SolverSearcher(new BreadthFirstSearch()), cache))
-		/*new SolverSearcher(new BreadthFirstSearch()), cache))*/;
-
-//	std::list<int> q;
-//	q.push_back(3);
-//	q.push_back(5);
-//	std::list<int> temp = q;
-//	temp.pop_back();
-//	for (int kI : q) {
-//		std::cout << kI << std::endl;
-//	}
+	my_p_server->open(5400,
+						   new MyClientHandler(new SolverSearcher(new BestFirstSearch<AStarCmp>()), cache));
 	return 0;
 }
