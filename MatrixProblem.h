e2ec4226cec5e5dbe556b33607e819448a5d8aa5
yoavst <48022265+213489628@users.noreply.github.com>
2020-01-30 23:39:23 +0200

    Update MatrixProblem.h

--- MatrixProblem.h
+++ MatrixProblem.h
@@ -5,20 +5,19 @@
 #ifndef ADVANCED1_HW4__MATRIXPROBLEM_H_
 #define ADVANCED1_HW4__MATRIXPROBLEM_H_
 #include "Searchable.h"
-//#include <utility>
 #include <vector>
 
 
-//#include <iostream>
 class MatrixProblem: public Searchable{
   std::vector<std::vector<int>>* matrix; //matrix of costs
-//  std::vector<std::vector<State<std::pair<int, int>>>>* states;
   State* init;
   State* goal;
  public:
   MatrixProblem(std::vector<std::vector<int>>* m, int xi, int yi, int xg, int yg);
   State* GetInitState() override{ return init;}
   bool isGoalState(State* s) override { return (*goal == s);}
+  
+  //returns all the possible succesors with updating cost and parent
   std::list<State*>* GetPossibleStates(State* s) override;
   std::string toString();
 
