52942a3d6afcce7cb0bd26547ab5099157d2f7b5
chen goldi <17chen07@gmail.com>
2020-01-30 17:06:07 +0200

    CG 30.1  - 17:04 added the parallel for checking

---
+++ MyParallelServer.cpp
@@ -0,0 +1,117 @@
+//
+// Created by חן גולדשטיין on 30/01/2020.
+//
+
+#include "MyParallelServer.h"
+
+#include "MyClientHandler.h"
+
+static void printSeconds() {
+	std::cout << "entered the thread" << std::endl;
+	int seconds = 1;
+	while (true) {
+		sleep(1);
+		std::cout << "a second passed:" << seconds << std::endl;
+		++seconds;
+	}
+}
+
+MyParallelServer::MyParallelServer() {
+//	this->clients_queue = new std::queue<int>;
+}
+int MyParallelServer::open(int port, ClientHandler *c) {
+	this->sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) {
+		std::cerr << "\n Socket creation error \n" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+
+	struct sockaddr_in serv_addr;
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons(port);
+
+	int bind_flag = bind(this->sockfd, (struct sockaddr *) (&serv_addr), sizeof(serv_addr));
+	int new_socket;
+	if (bind_flag < 0) {
+		std::cerr << "bind failed" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	if (listen(this->sockfd, 10) < 0) {
+		std::cerr << "listen error" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+
+	struct timeval tv;
+	tv.tv_sec = 60; // TODO: change to 120 seconds
+	tv.tv_usec = 0;
+	setsockopt(this->sockfd, SOL_SOCKET, SO_RCVTIMEO, (char *) &tv, sizeof(tv));
+	std::cout << "about to accept shit..." << std::endl;
+
+	/* TODO: THERE IS NO NEED FOR A QUEUE OF CLIENTS BECAUSE THE ACCEPT WILL ACCEPT ONLY AFTER FINISHING DEALING WITH THE PREVIOUS
+	 * TODO: CLIENT WE GOT THROUGH THE ACCEPT
+	 *
+	 * TODO: NEED TO FIND WHERE TO PUT THE STOP - HOW TO KNOW IF NO CLIENT WANTS TO JOIN.
+	 */
+	std::cout << "here we go accepting" << std::endl;
+//	std::thread seconds(printSeconds);
+//	seconds.detach();
+
+//	bool ok = true;
+//	while (ok) {
+//		struct fd_set rfds;
+//		struct timeval connection;
+//		FD_ZERO(&rfds);
+//		FD_SET(this->sockfd, &rfds);
+//		connection.tv_sec = 60;
+//		connection.tv_usec = 0;
+//		std::cout << "point 0" << std::endl;
+//
+//		new_socket = accept(this->sockfd, (struct sockaddr *) (&serv_addr), (socklen_t *) (&serv_addr));
+//		c->handleClient(new_socket);
+//
+//		int retval = select(1, &rfds, NULL, NULL, &connection);
+//		std::cout << "point 1" << std::endl;
+//		switch(retval) {
+//			case 0: //timeout
+//				std::cout << "point STOP" << std::endl;
+//				this->stop();
+//			case -1: // error
+//				std::cout << "point ERROR" << std::endl;
+//				ok = false;
+//				break;
+//			default: // can do accept on socket
+//				std::cout << "trying to accept" << std::endl;
+//				new_socket = accept(this->sockfd, (struct sockaddr *) (&serv_addr), (socklen_t *) (&serv_addr));
+//				std::cout << "about to handle a client" << std::endl;
+//				//goy stuff
+//				c->handleClient(new_socket);
+//				break;
+//		}
+//	}
+
+	while ((new_socket = accept(this->sockfd, (struct sockaddr *) (&serv_addr), (socklen_t *) (&serv_addr))) != -1) {
+
+		std::cout << "we accepted a client" << std::endl;
+		std::cout << "handling client" << std::endl;
+		new std::thread (&MyClientHandler::handleClient, c, new_socket); // TODO: this is something we need for the parallel
+		std::cout << "FINISHED handling client" << std::endl;
+		std::cout << "closed socket" << std::endl;
+
+	}
+
+	std::cout << "SHIT" << std::endl;
+	std::cout << "didn't accept" << std::endl;
+	this->stop();
+////	TODO: CHECK IF YOU NEED TO OPEN A THREAD FOR SOMETHING, IF YES THEN FOR WHAT' AND WHAT DO YOU DO WITH THE
+//// 	TODO: CLIENT_HANDLER IN THIS FUNCTION. in the end need to close the client's new socket.
+//	std::thread openServer(acceptClient, this);
+//	openServer.detach();
+	return 1;
+}
+int MyParallelServer::stop() {
+	close(this->sockfd);
+
+	return 1;
+}
+
---
+++ MyParallelServer.h
@@ -0,0 +1,23 @@
+//
+// Created by חן גולדשטיין on 30/01/2020.
+//
+
+#ifndef ADVANCED1_HW4_CMAKE_BUILD_DEBUG_MYPARALLELSERVER_H_
+#define ADVANCED1_HW4_CMAKE_BUILD_DEBUG_MYPARALLELSERVER_H_
+
+#include "Server.h"
+#include <thread>
+
+class MyParallelServer: public Server {
+ private:
+//	std::queue<int> *clients_queue;
+	int sockfd; // lets make it so it will take only one client (anyway we need PARALLEL)
+
+ public:
+	MyParallelServer();
+	int open(int port, ClientHandler* c) override ;
+	int stop() override ;
+	void closing_func(int signo);
+};
+
+#endif //ADVANCED1_HW4_CMAKE_BUILD_DEBUG_MYPARALLELSERVER_H_
